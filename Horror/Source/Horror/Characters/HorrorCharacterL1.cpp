aedf19838c700a56475b144b037ad42105947147
김민준 <79951019+kmj5515@users.noreply.github.com>
2026-04-21 20:52:55 +0900

    Level1 레벨에 필요한 기본 스크립트 생성

---
+++ Horror/Source/Horror/Characters/HorrorCharacterL1.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HorrorCharacterL1.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+AHorrorCharacterL1::AHorrorCharacterL1()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(GetCapsuleComponent());
+	CameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 60.0f));
+
+}
+
+// Called when the game starts or when spawned
+void AHorrorCharacterL1::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHorrorCharacterL1::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AHorrorCharacterL1::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Horror/Source/Horror/GameStates/GameStateL1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "GameStateL1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORROR_API AGameStateL1 : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
